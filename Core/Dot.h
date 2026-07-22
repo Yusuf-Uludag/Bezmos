9c98286f8a59e4cac40ecbebe55cbd3bd948d123
rendekar8 <abdullah.ozen@ug.bilkent.edu.tr>
2026-07-22 16:37:54 +0300

    Defined Dot Class

--- Core/Dot.h
+++ Core/Dot.h
@@ -1,4 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-class Dot : public sf::Vertex
-{};
+namespace Core
+{
+    enum class DotOrder
+    {
+        First,
+        Middle,
+        Last
+    };
+
+    struct Dot
+    {
+        sf::Vector2i position;
+        bool isSelected;
+
+        Dot(int x, int y)
+            : position(x, y)
+        {}
+
+        int getX() { return position.x; }
+        int getY() { return position.y; }
+        sf::Vector2i getPosition() { return position; }
+        void changePos(int x, int y)
+        {
+            sf::Vector2i newPos(x, y);
+            position = newPos;
+        }
+        void Select() { isSelected = true; }
+    };
+} // namespace Core
